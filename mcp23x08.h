3775abd8c00f26ed9bab6382903eec0adcea33f0
HokieGeek <andres.f.perez@gmail.com>
2015-11-23 12:44:59 -0500

    Refactoring to make use of the spi.h impl I have

--- mcp23x08.h
+++ mcp23x08.h
@@ -4,34 +4,20 @@
 #include <avr/interrupt.h>
 
 #define MCP23X08_SLAVE_BASE_ADDRESS (1 << 6)
-#define MCP23X08_SLAVE_ADDRESS_A0 0
-#define MCP23X08_SLAVE_ADDRESS_A1 0
-#define MCP23008_SLAVE_ADDRESS_A2 0
-#define MCP23S08_ADDRESS (MCP23X08_SLAVE_BASE_ADDRESS|(MCP23X08_SLAVE_ADDRESS_A1 << 2)|(MCP23X08_SLAVE_ADDRESS_A0 << 1))
-#define MCP23008_ADDRESS (MCP23X08_SLAVE_BASE_ADDRESS|(MCP23008_SLAVE_ADDRESS_A2 << 3)|(MCP23X08_SLAVE_ADDRESS_A1 << 2)|(MCP23X08_SLAVE_ADDRESS_A0 << 1))
-#define MCP23S08_OPCODE_WRITE MCP23S08_ADDRESS
-#define MCP23S08_OPCODE_READ (MCP23S08_ADDRESS|1)
-#define MCP23008_OPCODE_WRITE MCP23008_ADDRESS
-#define MCP23008_OPCODE_READ (MCP23008_ADDRESS|1)
 #define MCP23X08_REG_IODIR 0x00
 #define MCP23X08_REG_GPIO 0x09
 
-inline void SetChipSelectHigh(void);
+typedef struct {
+    SpiDevice* spi;
+    int address;
+} mcp23x08Device;
 
-inline void SetChipSelectLow(void);
+void MCP23S08_Send(mcp23x08Device* dev, uint8_t opcode, uint8_t registerAddress, uint8_t data);
 
-inline void ToggleSerialClock(void);
+inline void MCP23S08_GpioWrite(mcp23x08Device* dev, uint8_t data);
 
-void SpiSendByte(uint8_t data);
+inline void MCP23S08_IodirWrite(mcp23x08Device* dev, uint8_t dirs);
 
-void SpiMasterInit(void);
-
-void MCP23S08_Send(uint8_t opcode, uint8_t registerAddress, uint8_t data);
-
-inline void MCP23S08_GpioWrite(uint8_t data);
-
-inline void MCP23S08_IodirWrite(uint8_t dirs);
-
-void MCP23X08_Init(void);
+mcp23x08Device* MCP23S08_Init(int chipSelect, int serialClock, int serialDataInput, int addressA0, int addressA1);
 
 #endif // __MCP23X08__H__
