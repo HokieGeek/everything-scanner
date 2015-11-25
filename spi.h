5417902e48e9609bfa6c06d72e599ea3eabaffe6
HokieGeek <andres.f.perez@gmail.com>
2015-11-25 08:59:05 -0500

    Cleaning up this file and correcting Init3WireSpiDevice

--- spi.h
+++ spi.h
@@ -3,11 +3,6 @@
 
 #include <stdint.h>
 
-// typedef enum {
-//     3WIRE,
-//     5WIRE
-// } SpiDeviceTypeEnum;
-
 typedef struct {
     int chipSelect;
     int serialClock;
@@ -19,20 +14,8 @@ void SetChipSelectLow(SpiDevice *const dev);
 void ToggleSerialClock(SpiDevice *const dev);
 void SpiSendByte(SpiDevice *const dev, uint8_t data);
 void SpiWriteBytes(SpiDevice *const dev, int numBytes, uint8_t data[]);
-void Init3WireSpiDevice(int chipSelect, int serialClock, int serialDataInput, SpiDevice *dev);
+void Init3WireSpiDevice(SpiDevice *dev);
 
-/*
-typedef struct {
-    int chipSelect;
-    // TODO: is master
-} SpiDevice2;
-
-void SetChipSelectHigh2(SpiDevice2 *const dev);
-void SetChipSelectLow2(SpiDevice2 *const dev);
-
-SpiDevice2 *const InitSpiMaster(int chipSelect);
-uint8_t SpiSend(SpiDevice2 *const dev, uint8_t data);
-uint8_t SpiSend16(SpiDevice2 *const dev, uint8_t byte1, uint8_t byte2);
-*/
+// uint8_t SpiSendByte2(SpiDevice *const dev, uint8_t data);
 
 #endif //__SPI_H__
